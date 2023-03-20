8be78602922cb815a8a998fe8de0c12db07aac32
Sheldon <aneln700@gmail.com>
2023-03-20 19:00:55 +0300

    Comm

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class BmiService {
+public class BmiService {
+    public double calculate(int kg, double m) {
+        double index = kg/(m*m);
+        return (int) index;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BmiService service = new BmiService();
+        double index = service.calculate(68, 1.87);
+        System.out.println(index);
     }
 }
